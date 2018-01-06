f8c54d42550cfa9f52ba42312bdeda0438b89a5f
Mohammed Saad <34467824+Saad789@users.noreply.github.com>
2018-01-06 16:28:11 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,32 +1,23 @@
-class Cone1{
-	  String name;
-	  
-	  void calc(int a, int b){
-	  System.out.println("the sum of two numbers is:"+ (a + b));
-	 }
+
+class Student {
+	
+private int id;
+	
+Student(int id){
+	this.id = id;
+    }
+
+void displayInfo(){
+	 System.out.println("ID:"+id);
+    }
 }
 
-class Ctwo2 extends Cone1{
-	void calc(int a, int b){
-	super.calc(10,20);
-	System.out.println("the mul of two numbers is:"+ (a * b));
-	 }
-	
-	
-	  void div(int a , int b){
-	  System.out.println("the div of two numbers is:"+ (a / b));
-	}
+class Test{
+	  public static void main(String[] args) {
+		
+	  Student st = new Student(45);
+	  st.displayInfo();
+    }
 }
 
-public class Test {
-	
-	public static void main(String[] args) {
-    // case1:Creating object for SuperClass
-	// and hold in SuperClass reference.
-	
-	Ctwo2 c = new Ctwo2();
-	c.calc(100,200);
-	c.div(10, 2);
-	}
 
-}
---
+++ Test06.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+class Test06 {
+
+public static void main(String[] args) {
+		
+	    HashSet hs = new HashSet();
+		
+		
+		hs.add(10);
+		hs.add(20);
+		hs.add(30);
+		
+		System.out.println(hs);
+		
+	
+
+	}
+
+}

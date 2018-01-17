3698c184804602aa0dc35d3170aa6272be47e133
Mohammed Saad <34467824+Saad789@users.noreply.github.com>
2018-01-17 16:25:36 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,14 @@
+import java.io.PrintStream;
+
+public class Sample {
+
+   static PrintStream out = new PrintStream(System.out);
+	
+	public static void main(String[] args) {
+		Sample.out.println("hi");
+		
+		
+
+	}
+
+}
